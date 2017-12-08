ad35b13a00bddd41bf997d4d930f05938a7138c7
SuchJakub <32745890+SuchJakub@users.noreply.github.com>
2017-12-08 09:31:24 +0100

    Add files via upload

---
+++ SuchJakub-8.12.2017-infa/c++/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+for (i=1;i<=22;i=i+3)
+    cout<<i<<" ";
+    return 0;
+}
---
+++ SuchJakub-8.12.2017-infa/c++/zadanie nr2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,suma=0;
+for (i=1;i<=50;i=i++)
+suma=suma+i;
+cout<<suma;
+    return 0;
+}
